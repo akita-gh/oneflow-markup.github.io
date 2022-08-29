708ee3b7e286cef19c1c573cad7a90e858d80da1
1Alusha1 <alusha9636@gmail.com>
2022-08-29 17:42:24 +0300

    integrations helped

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,14 +7,11 @@ var marginRight = parseInt(getComputedStyle(slide, true).marginRight);
 let slideWidth = slide.offsetWidth + marginLeft + marginRight;
 let sliderWidth = slider.children.length * slideWidth;
 
-console.log(slideWidth);
-
 let width = 0;
 let count = 0;
 setInterval(() => {
-  console.table(slider.children.length, count);
   count++;
-  if (count < slider.children.length -3) {
+  if (count < slider.children.length - 3) {
     width += slideWidth;
     console.table(width, slideWidth, sliderWidth);
     slider.style.cssText = `transform:translateX(-${width}px)`;
